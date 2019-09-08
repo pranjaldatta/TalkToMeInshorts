3d69219cedd7fda7dfdaca0ad5ca183253cd9e20
Pranjal  Datta <pranjaldatta99@gmail.com>
2019-09-09 03:03:53 +0530

    updated keys.js

--- server/assets/keys.js
+++ server/assets/keys.js
@@ -1,6 +1,6 @@
 var awsPollyAuth = {
-    accessKey : "AKIA23GUCI43SI77E6H6", //put your own acceessKey here
-    secretAccessKey : "FFgBHyRVJ7h8qR8ck7qtxoxR4l18Lj9GrNi+nvHG",    //put your own secretAccessKey here
+    accessKey : "", //put your own acceessKey here
+    secretAccessKey : "",    //put your own secretAccessKey here
     region: "us-east-1"
 }
 
